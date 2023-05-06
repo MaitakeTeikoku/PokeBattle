2c68255d5dafd12d0f357e86b12c50f4301ff4a1
MaitakeTeikoku <129482574+MaitakeTeikoku@users.noreply.github.com>
2023-05-07 01:57:55 +0900

    Add files via upload

--- js/typeEffective.js
+++ js/typeEffective.js
@@ -1,5 +1,34 @@
-// タイプの数
-const numType = 18;
+// タイプの総数
+const typeSum = {
+    "normal": 130,
+    "fire": 80,
+    "water": 154,
+    "grass": 122,
+    "electric": 68,
+    "ice": 48,
+    "fighting": 72,
+    "poison": 79,
+    "ground": 75,
+    "flying": 109,
+    "psychic": 99,
+    "bug": 92,
+    "rock": 73,
+    "ghost": 62,
+    "dragon": 65,
+    "dark": 69,
+    "steel": 63,
+    "fairy": 63
+}
+
+// タイプの合計数
+const numTypeSum = Object.keys(typeSum).length;
+
+// ポケモンの合計数
+let numPokeCount = 0;
+Object.keys(typeSum).forEach((key) => {
+    numPokeCount += typeSum[key];
+});
+const numPokeSum = numPokeCount;
 
 
 
@@ -24,7 +53,8 @@ async function displayTypeEffective() {
     
     let listTypeData = [];
 
-    for (let i = 0;  i < numType; i++) {
+    // 単タイプ
+    for (let i = 0;  i < numTypeSum; i++) {
         let type1Res = await fetch("https://pokeapi.co/api/v2/type/" + (i + 1));
         let type1Data = await type1Res.json();
 
@@ -33,7 +63,7 @@ async function displayTypeEffective() {
 
         // タイプ名を表示
         let td0List = tr.insertCell(-1);
-        td0List.innerHTML = type1Name;
+        td0List.innerHTML = type1Name + "<br>" + typeSum[type1Data['name']] + "匹";
         td0List.classList.add("typeEffectiveTd0");
 
         let double_damage_from = await getTypeEffectiveList(type1Data, "double_damage_from");
@@ -47,15 +77,26 @@ async function displayTypeEffective() {
 
         let typeClacList = await typeCalc(double_damage_to, half_damage_to, no_damage_to, double_damage_from, half_damage_from, no_damage_from);
 
-        listTypeData.push([type1Name, point, typeClacList]);
+        let typePointSum = 1;
+        let typeSumPointSum = 0;
+        for (let k = 0; k < typeClacList.length; k++) {
+            let typePoint = typeClacList[k]["typePoint"];
+            typePointSum *= parseFloat(typePoint);
+            
+            let typeSumPoint = typeClacList[k]["typeSumPoint"];
+            typeSumPointSum += parseFloat(typeSumPoint);
+        }
+
+        listTypeData.push([type1Name, point, typeClacList, typePointSum.toLocaleString(undefined, {maximumFractionDigits: 3}), (typeSumPointSum / numPokeSum).toLocaleString(undefined, {maximumFractionDigits: 3})]);
     }
 
-    for (let i = 0;  i < numType; i++) {
-        for(j = i + 1; j < numType; j++) {
+    // 複合タイプ
+    for (let i = 0;  i < numTypeSum; i++) {
+        for(j = i + 1; j < numTypeSum; j++) {
             let type1Res = await fetch("https://pokeapi.co/api/v2/type/" + (i + 1));
             let type1Data = await type1Res.json();
 
-            // タイプの名前を取得
+            // タイプ1の名前を取得
             let type1Name = await getTypeNameJpn(type1Data['name']);
 
             let double_damage_from = await getTypeEffectiveList(type1Data, "double_damage_from");
@@ -70,7 +111,7 @@ async function displayTypeEffective() {
             let type2Res = await fetch("https://pokeapi.co/api/v2/type/" + (j + 1));
             let type2Data = await type2Res.json();
 
-            // タイプの名前を取得
+            // タイプ2の名前を取得
             let type2Name = await getTypeNameJpn(type2Data['name']);
 
             double_damage_from = double_damage_from.concat(await getTypeEffectiveList(type2Data, "double_damage_from"));
@@ -80,16 +121,34 @@ async function displayTypeEffective() {
             no_damage_from = no_damage_from.concat(await getTypeEffectiveList(type2Data, "no_damage_from"));
             no_damage_to = no_damage_to.concat(await getTypeEffectiveList(type2Data, "no_damage_to"));
 
+
+
             point = - double_damage_from.length * pointEffectiveSuper + double_damage_to.length * pointEffectiveSuper + half_damage_from.length * pointEffectiveNotVery - half_damage_to.length * pointEffectiveNotVery + no_damage_from.length * pointEffectiveNot - no_damage_to.length * pointEffectiveNot;
 
             let typeClacList = await typeCalc(double_damage_to, half_damage_to, no_damage_to, double_damage_from, half_damage_from, no_damage_from);
+            
+            let typePointSum = 1;
+            let typeSumPointSum = 0;
+            for (let k = 0; k < typeClacList.length; k++) {
+                let typePoint = typeClacList[k]["typePoint"];
+                typePointSum *= parseFloat(typePoint);
+                
+                let typeSumPoint = typeClacList[k]["typeSumPoint"];
+                typeSumPointSum += parseFloat(typeSumPoint);
+            }
 
-            listTypeData.push([type1Name + "/<br>" + type2Name, point, typeClacList]);
+            listTypeData.push([type1Name + "/<br>" + type2Name, point, typeClacList, typePointSum.toLocaleString(undefined, {maximumFractionDigits: 3}), (typeSumPointSum / numPokeSum).toLocaleString(undefined, {maximumFractionDigits: 3})]);
         }
     }
 
     // データを並び替え
     listTypeData.sort(function(a,b){
+        // 得点が高い順
+        if(a[4] > b[4]) return -1;
+        if(a[4] < b[4]) return 1;
+        // 得点が高い順
+        if(a[3] > b[3]) return -1;
+        if(a[3] < b[3]) return 1;
         // 得点が高い順
         if(a[1] > b[1]) return -1;
         if(a[1] < b[1]) return 1;
@@ -101,41 +160,56 @@ async function displayTypeEffective() {
         tr = tableList.insertRow(-1);
 
         // タイプ名を表示
-        let td1List = tr.insertCell(-1);
+        let td1List = tr.insertCell(0);
         td1List.innerHTML = listTypeData[i][0];
         td1List.classList.add("typeEffectiveTd1");
-        // 得点を表示
-        let td2List = tr.insertCell(-1);
-        td2List.innerHTML = listTypeData[i][1];
+
+        // 平均倍率を表示
+        let td2List = tr.insertCell(1);
+        td2List.innerHTML = "<b>×" + listTypeData[i][4] + "</b>";
         td2List.classList.add("typeEffectiveTd2");
 
+        // 得点を表示
+        let td3List = tr.insertCell(2);
+        td3List.innerHTML = "×" + listTypeData[i][3];
+        td3List.classList.add("typeEffectiveTd3");
+
+        // 得点を表示
+        let td4List = tr.insertCell(3);
+        let pointValue = listTypeData[i][1];
+        if (listTypeData[i][1] > 0) {
+            pointValue = "+" + listTypeData[i][1];
+        }
+        td4List.innerHTML = pointValue;
+        td4List.classList.add("typeEffectiveTd4");
+        
+        // 個別得点を表示
         for (let j = 0; j < listTypeData[i][2].length; j++) {
-            let typePoint = (listTypeData[i][2][j]["damage_to"] / listTypeData[i][2][j]["damage_from"]).toLocaleString(undefined, {maximumFractionDigits: 3});
-            let typeEffective = typePoint + "<br>(" + listTypeData[i][2][j]["damage_to"] + "/<br>" + listTypeData[i][2][j]["damage_from"] + ")";
+            let typePoint = listTypeData[i][2][j]["typePoint"];
 
             // 得点を表示
-            let td3List = tr.insertCell(-1);
-            td3List.innerHTML = typeEffective;
-            td3List.classList.add("typeEffectiveTd3");
+            let td5List = tr.insertCell(-1);
+            td5List.innerHTML = "<b>×" + typePoint + "</b><br>(" + listTypeData[i][2][j]["damage_to"] + "/" + listTypeData[i][2][j]["damage_from"] + ")<br>[" + listTypeData[i][2][j]["typeSumPoint"].toLocaleString(undefined, {maximumFractionDigits: 3}) + "]";
+            td5List.classList.add("typeEffectiveTd5");
 
             if (typePoint >= 10) {
-                td3List.style.backgroundColor = "#ffcccc";
+                td5List.style.backgroundColor = "#ffcccc";
             } else if (typePoint >= 4) {
-                td3List.style.backgroundColor = "#ffe5cc";
+                td5List.style.backgroundColor = "#ffe5cc";
             } else if (typePoint >= 2) {
-                td3List.style.backgroundColor = "#ffffcc";
+                td5List.style.backgroundColor = "#ffffcc";
             } else if (typePoint > 1) {
-                td3List.style.backgroundColor = "#e5ffcc";
+                td5List.style.backgroundColor = "#e5ffcc";
             } else if (typePoint == 1) {
-                td3List.style.backgroundColor = "#ccffcc";
+                td5List.style.backgroundColor = "#ccffcc";
             } else if (typePoint >= 0.5) {
-                td3List.style.backgroundColor = "#ccffe5";
+                td5List.style.backgroundColor = "#ccffe5";
             } else if (typePoint >= 0.25) {
-                td3List.style.backgroundColor = "#ccffff";
+                td5List.style.backgroundColor = "#ccffff";
             } else if (typePoint >= 0.25) {
-                td3List.style.backgroundColor = "#cce5ff";
+                td5List.style.backgroundColor = "#cce5ff";
             }else {
-                td3List.style.backgroundColor = "#cce5ff";
+                td5List.style.backgroundColor = "#cce5ff";
             }
         }
     }
@@ -169,7 +243,7 @@ async function getTypeNameJpn(typeName) {
 
 async function typeCalc(double_damage_to, half_damage_to, no_damage_to, double_damage_from, half_damage_from, no_damage_from) {
     let typeClacList = []; 
-    for (let i = 0;  i < numType; i++) {
+    for (let i = 0;  i < numTypeSum; i++) {
 
         let typeRes = await fetch("https://pokeapi.co/api/v2/type/" + (i + 1));
         let typeData = await typeRes.json();
@@ -210,7 +284,16 @@ async function typeCalc(double_damage_to, half_damage_to, no_damage_to, double_d
             }
         }
 
-        typeClacList[i] = {"typeName": typeName, "damage_to": damage_to.toLocaleString(undefined, {maximumFractionDigits: 3}), "damage_from": damage_from.toLocaleString(undefined, {maximumFractionDigits: 3})};
+        let typePoint = damage_to / damage_from;
+        let typeSumPoint = typeSum[typeName] * typePoint;
+
+        typeClacList[i] = {
+            "typeName": typeName,
+            "damage_to": damage_to.toLocaleString(undefined, {maximumFractionDigits: 3}),
+            "damage_from": damage_from.toLocaleString(undefined, {maximumFractionDigits: 3}),
+            "typePoint": typePoint.toLocaleString(undefined, {maximumFractionDigits: 3}),
+            "typeSumPoint": typeSumPoint.toLocaleString(undefined, {maximumFractionDigits: 3})
+        };
     }
 
     return typeClacList;
