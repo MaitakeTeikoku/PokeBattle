6386fb66f9c70751c8314b97a0c268a10757d0f8
MaitakeTeikoku <129482574+MaitakeTeikoku@users.noreply.github.com>
2023-06-06 18:07:46 +0900

    Delete GAS-util.js

--- js/GAS-util.js
+++
@@ -1,9 +0,0 @@
-// min以上max以下の整数値の乱数を返す関数
-function intRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// 小数点を切り捨てる関数
-function floorDecimal(num, digit) {
-    return Math.floor(num * (10 ** digit)) / (10 ** digit);
-}
